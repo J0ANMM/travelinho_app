1aeea3f820d7d4de6e6192028a82a255f4e9d543
Roger Paris <roger.parisvicent@gmail.com>
2018-02-03 12:31:18 +0100

    improve webview with correct statusbar for both platforms

--- src/components/containers/mainAppContainer.js
+++ src/components/containers/mainAppContainer.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import { WebView, StyleSheet } from 'react-native';
+import { WebView, StyleSheet, Linking, View, StatusBar, Platform } from 'react-native';
+
+const domain = 'travelinho.com'
 
 class MainAppContainer extends Component {
   render() {
     return (
-      <WebView
-        style={styles.container}
-        source={{ uri: 'https://www.google.com' }}
-        onLoadStart={() => {
-          console.log('onLoadStart')
-        }}
-        onLoad={() => {
-          console.log('onLoad')
-        }}
-        onError={console.log}
-      />
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor="#0A3511"
+        />
+        <WebView
+          ref="webview"
+          style={styles.webview}
+          source={{ uri: 'http://www.travelinho.com' }}
+          onNavigationStateChange={(event) => {
+            console.log(event)
+            if (!event.url.includes(domain)) {
+              this.refs.webview.stopLoading();
+              Linking.openURL(event.url);
+            }
+          }}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+    marginTop: (Platform.OS === 'ios') ? 20 : 0
   }
 });
 
